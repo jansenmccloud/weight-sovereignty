37fade529d6de5b2cbc016f0eb93b1892fb2be95
jansenmccloud <jansenmccloud@gmail.com>
2026-03-05 00:40:04 +0100

    added riverpod dependency

--- flutter/weight_sovereignty/lib/main.dart
+++ flutter/weight_sovereignty/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weight_sovereignty/src/presentation/app_root.dart';
 
 void main() {
-  runApp(const AppRoot());
+  runApp(
+    ProviderScope(child: AppRoot())
+  );
 }
